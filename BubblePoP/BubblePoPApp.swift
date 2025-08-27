56b9701461a3f74890a38b9c2efff1d8781c57cf
Mrityunjay Vashisth <mrityunjayvashisth@gmail.com>
2025-08-27 11:44:46 +0530

    Initial Commit

---
+++ BubblePoP/BubblePoPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePoPApp.swift
+//  BubblePoP
+//
+//  Created by Mrityunjay Vashisth on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct BubblePoPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubblePoPTests/BubblePoPTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePoPTests.swift
+//  BubblePoPTests
+//
+//  Created by Mrityunjay Vashisth on 27/08/25.
+//
+
+import Testing
+@testable import BubblePoP
+
+struct BubblePoPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
